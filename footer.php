e858133d4322c32a3e5fa891bfe8f521c0f38a1a
Amy Benson <avbenson@gmail.com>
2016-10-02 16:05:31 +0100

    basic footer

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
@@ -11,16 +10,37 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sgp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sgp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sgp' ), 'sgp', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="site-footer" role="contentinfo">
+		<div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <h2>About us</h2>
+       
+        
+          <h2>Get involved</h2>
+       
+        
+          <h2>News</h2>
+          <h2>Events</h2>
+          <h2>Donate</h2>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+          <h2>Connect with us</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8">
+         <p class="site-footer__small-print">Promoted by Eamonn Ward, 73 Eskdale Rd, Sheffield, S6 1SL on behalf of Sheffield Green Party</p>
+         <p class="site-footer__small-print">Copyright © <?php echo date("Y") ?> Sheffield Green Party</p>
+        </div>
+        <div class="col-sm-4">
+         <p class="site-footer__small-print">Terms and conditions       Privacy policy</p>
+        </div>
+      </div>
+    </div>
+	</footer>
 
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 
 </body>
 </html>
