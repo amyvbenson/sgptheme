907b1175c949c5a7fef0241d1b6664bb2ee77bf6
Amy Benson <avbenson@gmail.com>
2017-08-11 18:54:49 +0100

    Favicons

--- header.php
+++ header.php
@@ -16,6 +16,12 @@
   <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/safari-pinned-tab.svg" color="#6ab023">
+  <meta name="theme-color" content="#ffffff">
   <script>document.querySelector('html').className += ' js';</script>
 </head>
 <body <?php body_class(); ?>>
