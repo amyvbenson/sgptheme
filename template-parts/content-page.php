ed7554bb7b4c159d530e7b263689fa7c568af657
Amy Benson <avbenson@gmail.com>
2017-09-21 19:19:49 +0100

    Show related posts from custom field on pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Template part for displaying page content in page.php.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package sgp
- */
+* Template part for displaying page content in page.php.
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package sgp
+*/
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="page-header">
-		<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="page-content">
-		<?php
-			the_content();
-		?>
-	</div>
+  <header class="page-header">
+    <?php the_title('<h1 class="page-title">', '</h1>'); ?>
+  </header><!-- .entry-header -->
+  <div class="page-content">
+    <?php
+    the_content();
+    ?>
+    <?php get_template_part('template-parts/posts/related-posts'); ?>
+</div>
 </article>
---
+++ template-parts/posts/related-posts.php
@@ -0,0 +1,11 @@
+<?php $cat = get_post_meta($post->ID, 'category', true); ?>
+    <?php if ($cat) : ?>
+      <h2 class="home-section__heading">Related posts</h2>
+        <?php $category_posts = sgp_category_posts($cat, 5);
+        while ($category_posts->have_posts()) :
+            $category_posts->the_post();
+            get_template_part('template-parts/posts/post-preview-simple');
+        endwhile; ?>
+        <a href="/category/type/<?php echo $cat;?>" class="home-section__more-link primary-btn">More posts</a>
+    <?php endif; ?>
+<?php wp_reset_postdata() ?>
