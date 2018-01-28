2955effb6741bbc4a47acdda403bb95ec023d5b8
Amy Benson <avbenson@gmail.com>
2018-01-28 11:38:42 +0000

    Added GA

--- header.php
+++ header.php
@@ -23,6 +23,17 @@
   <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/safari-pinned-tab.svg" color="#6ab023">
   <meta name="theme-color" content="#ffffff">
   <script>document.querySelector('html').className += ' js';</script>
+  <?php if (!current_user_can('administrator') && !current_user_can('editor')): ?>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-113193366-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+  <?php endif; ?>
 </head>
 <body <?php body_class(); ?>>
 <div hidden aria-hidden="true"><?php get_template_part( 'images/icons/symbol-defs.svg' ); ?></div>
