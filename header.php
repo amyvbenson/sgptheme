c6da33d646b55956a50d17c9b3d2ff66e48c9e10
Amy Benson <avbenson@gmail.com>
2018-02-17 12:43:24 +0000

    Revert to manifest.json

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
-  <link rel="manifest" href="/site.webmanifest">
+  <link rel="manifest" href="/manifest.json">
   <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/safari-pinned-tab.svg" color="#6ab023">
   <meta name="theme-color" content="#ffffff">
   <script>document.querySelector('html').className += ' js';</script>
