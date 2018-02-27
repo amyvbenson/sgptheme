4adbe48e5f5c8194f73c12c1a4ab49b6049639a9
Amy Benson <avbenson@gmail.com>
2018-02-27 11:43:47 +0000

    Newsletters content type style

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,6 +15,14 @@
 		<?php if (in_category('letters-to-the-press')): ?>
 			<a href="/category/letters-to-the-press/" class="page-header__letters-icon">Letters to the press</a>
 		<?php endif; ?>
+
+		<?php if (in_category('newsletters')): ?>
+			<a href="/category/newsletters/" class="page-header__newsletter">
+				<span class="page-header__newsletter-heading">The Sheffield Green</span>
+				<span class="page-header__newsletter-tagline">Your local Green Party newsletter</span>
+			</a>
+		<?php endif; ?>
+
 		<?php
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -22,7 +30,7 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) : ?>
+		if ( 'post' === get_post_type() && !in_category('newsletters') ) : ?>
 			<div class="page-meta">
 				<p class="post__date">
 	        <?php the_time(__('jS', 'default')) ?>  <?php the_time(__('F', 'default')) ?> <?php the_time(__('Y', 'default')) ?>
