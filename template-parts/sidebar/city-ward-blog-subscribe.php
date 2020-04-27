ae47b7abe8392c00496923a21ae0886ffe6bd501
Amy Benson <avbenson@gmail.com>
2020-04-27 17:41:07 +0100

    files

---
+++ template-parts/sidebar/city-ward-blog-subscribe.php
@@ -0,0 +1,17 @@
+<div class="content-block">
+  <h2><label for="mce-EMAIL">Subscribe for updates</label></h2>
+  <p>Sign up for regular updates from the City Ward blog:</p>
+  <div id="mc_embed_signup" class="mc-sidebar">
+    <form action="https://sheffieldgreenparty.us3.list-manage.com/subscribe/post?u=2f0e13e14e68429fb33a1876b&amp;id=a36f489b56" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+        <div class="mc-input-wrapper">
+          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+          <input type="checkbox" value="2" name="group[27069][2]" id="mce-group[27069]-27069-1" checked hidden>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2f0e13e14e68429fb33a1876b_a36f489b56" tabindex="-1" value=""></div>
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
