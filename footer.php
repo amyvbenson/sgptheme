9bc0364a1f210deb9132e47e434c6b635c638b3d
Amy Benson <avbenson@gmail.com>
2020-04-27 17:10:14 +0100

    Add group to footer subscribe form

--- footer.php
+++ footer.php
@@ -30,9 +30,10 @@
   <label for="mce-EMAIL">Subscribe to weekly news updates:</label>
   <div class="mc-input-wrapper">
 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2f0e13e14e68429fb33a1876b_a36f489b56" tabindex="-1" value=""></div>
-    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+  <input type="checkbox" value="1" name="group[27069][1]" id="mce-group[27069]-27069-0" checked hidden>
+  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2f0e13e14e68429fb33a1876b_a36f489b56" tabindex="-1" value=""></div>
+  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
 </div>
     </div>
 </form>
